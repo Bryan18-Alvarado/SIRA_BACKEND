d10ff32b68ff4a0834105559cf5017c0486abb6e
katia-silva09 <katiajuliethsilvareyes09@gmail.com>
2025-04-19 02:18:42 -0600

    add paginacion al modulo de calificaciones

--- src/modules/calificaciones/controllers/calificaciones.controller.ts
+++ src/modules/calificaciones/controllers/calificaciones.controller.ts
@@ -1,18 +1,49 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { CalificacionesService } from '../services/calificaciones.service';
-import { CreateCalificacionDto } from '../dto/calificacion.dto/calificacion.dto';
+import {
+  CreateCalificacionDto,
+  UpdateCalificacionDto,
+} from '../dto/calificacion.dto/calificacion.dto';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Controller('calificaciones')
 export class CalificacionesController {
   constructor(private readonly calificacionesService: CalificacionesService) {}
 
   @Get()
-  getAllCalificaciones() {
-    return 'Calificaciones';
+  findAll(@Query() paginationDto: PaginationDto) {
+    return this.calificacionesService.findAll(paginationDto);
   }
 
   @Post()
-  createCalificacion(@Body() createCalificacionDto: CreateCalificacionDto) {
+  create(@Body() createCalificacionDto: CreateCalificacionDto) {
     return this.calificacionesService.create(createCalificacionDto);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.calificacionesService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: number,
+    @Body() updatecalificacionDto: UpdateCalificacionDto, // O puedes crear un UpdateCalificacionDto
+  ) {
+    return this.calificacionesService.update(id, updatecalificacionDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return this.calificacionesService.remove(id);
   }
 }
--- src/modules/calificaciones/services/calificaciones.service.ts
+++ src/modules/calificaciones/services/calificaciones.service.ts
@@ -1,15 +1,35 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Calificacion } from '../entities/calificacion.entity';
-import { CreateCalificacionDto } from '../dto/calificacion.dto/calificacion.dto';
+import {
+  CreateCalificacionDto,
+  UpdateCalificacionDto,
+} from '../dto/calificacion.dto/calificacion.dto';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Injectable()
 export class CalificacionesService {
+  private readonly logger = new Logger('CalificacionesService');
+
   constructor(
     @InjectRepository(Calificacion)
     private readonly calificacionRepository: Repository<Calificacion>,
   ) {}
+
+  async findAll(paginationDto: PaginationDto) {
+    const { limit = 3, offset = 0 } = paginationDto;
+    return this.calificacionRepository.find({
+      take: limit,
+      skip: offset,
+    });
+  }
 
   async create(createCalificacionDto: CreateCalificacionDto) {
     try {
@@ -17,11 +37,65 @@ export class CalificacionesService {
         createCalificacionDto,
       );
       await this.calificacionRepository.save(calificacion);
-
       return calificacion;
     } catch (error) {
-      console.log(error);
-      throw new InternalServerErrorException('Error al crear la calificación');
+      //   console.error(error);
+      //   throw new InternalServerErrorException('Ayuda!');
+      this.handleDBException(error);
     }
+  }
+
+  async update(id: number, updateCalificacionDto: UpdateCalificacionDto) {
+    const calificacion = await this.calificacionRepository.findOne({
+      where: { grades_id: id },
+    });
+
+    if (!calificacion) {
+      throw new NotFoundException(`Calificación con id ${id} no encontrada`);
+    }
+    try {
+      this.calificacionRepository.merge(calificacion, updateCalificacionDto);
+      await this.calificacionRepository.save(calificacion);
+      return {
+        message: 'Registro actualizado con éxito',
+        data: calificacion,
+      };
+    } catch (error) {
+      this.handleDBException(error);
+    }
+  }
+
+  async remove(id: number) {
+    const exists = await this.calificacionRepository.existsBy({
+      grades_id: id,
+    });
+    if (!exists) {
+      throw new NotFoundException(`Calificación con id ${id} no encontrada`);
+    }
+    await this.calificacionRepository.softDelete({ grades_id: id }); // Soft delete deja fecha de eliminacion
+    return {
+      message: `Calificación con ID ${id} eliminada con éxito`,
+      deletedAt: new Date(),
+    };
+  }
+
+  async findOne(id: number) {
+    const calificacion = await this.calificacionRepository.findOneBy({
+      grades_id: id,
+    });
+    if (!calificacion) {
+      throw new NotFoundException(`Calificación con id ${id} no encontrada`);
+    }
+    return calificacion;
+  }
+
+  private handleDBException(error: any) {
+    if (error.code === '23505') throw new BadRequestException(error.detail);
+
+    this.logger.error(error);
+
+    throw new InternalServerErrorException(
+      'Ocurrió un error inesperado. Intente más tarde',
+    );
   }
 }
