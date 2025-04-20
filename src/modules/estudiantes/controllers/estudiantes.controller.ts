53ec829df4a2bc7760c4fefca0e82874e481d94a
katia-silva09 <katiajuliethsilvareyes09@gmail.com>
2025-04-19 23:02:18 -0600

    add pagination a estudiantes

--- src/modules/estudiantes/controllers/estudiantes.controller.ts
+++ src/modules/estudiantes/controllers/estudiantes.controller.ts
@@ -1,17 +1,48 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
 import { EstudiantesService } from '../services/estudiantes.service';
-import { CreateEstudianteDto } from '../dto/estudiantes.dto';
+import {
+  CreateEstudianteDto,
+  UpdateEstudianteDto,
+} from '../dto/estudiantes.dto';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('estudiantes')
 export class EstudiantesController {
-  constructor(private readonly estudianeteService: EstudiantesService) {}
+  constructor(private readonly estudianteService: EstudiantesService) {}
   @Get()
-  getEstudiantesAll() {
-    return 'Estudiantes';
+  getEstudiantesAll(@Query() paginationDto: PaginationDto): Promise<any> {
+    return this.estudianteService.findAll(paginationDto);
   }
 
   @Post()
   createEstudiante(@Body() createEstudianteDto: CreateEstudianteDto) {
-    return this.estudianeteService.create(createEstudianteDto);
+    return this.estudianteService.create(createEstudianteDto);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.estudianteService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: number,
+    @Body() updateEstudianteDto: UpdateEstudianteDto, // O puedes crear un UpdateEstudianteDto
+  ) {
+    return this.estudianteService.update(id, updateEstudianteDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return this.estudianteService.remove(id);
   }
 }
