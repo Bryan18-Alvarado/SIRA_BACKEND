0d662c7cff8cd283867573b7eb7b806f38a7e683
katia-silva09 <katiajuliethsilvareyes09@gmail.com>
2025-04-19 23:11:40 -0600

    paginacion de studentcourses

--- src/modules/student-courses/controllers/studentcourses.controller.ts
+++ src/modules/student-courses/controllers/studentcourses.controller.ts
@@ -1,18 +1,46 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { CreateStudentCourseDto } from '../dto/studentcourse.dto';
 import { StudentCoursesService } from '../services/studentcourses.service';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Controller('studentcourses')
 export class StudentCoursesController {
   constructor(private readonly studentCoursesService: StudentCoursesService) {}
 
   @Get()
-  getAllStudentCourses() {
-    return 'Student Courses';
+  findAll(@Query() paginationDto: PaginationDto) {
+    return this.studentCoursesService.findAll(paginationDto);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.studentCoursesService.findOne(id);
   }
 
   @Post()
-  createStudentCourse(@Body() createStudentCourseDto: CreateStudentCourseDto) {
+  create(@Body() createStudentCourseDto: CreateStudentCourseDto) {
     return this.studentCoursesService.create(createStudentCourseDto);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: number,
+    @Body() updateDto: CreateStudentCourseDto, // actualizar el dto
+  ) {
+    return this.studentCoursesService.update(id, updateDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return this.studentCoursesService.remove(id);
   }
 }
--- src/modules/student-courses/services/studentcourses.service.ts
+++ src/modules/student-courses/services/studentcourses.service.ts
@@ -1,15 +1,32 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { StudentCourse } from '../entities/studentcourse.entity';
 import { CreateStudentCourseDto } from '../dto/studentcourse.dto';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Injectable()
 export class StudentCoursesService {
+  private readonly logger = new Logger('StudentCoursesService');
+
   constructor(
     @InjectRepository(StudentCourse)
     private readonly studentCourseRepository: Repository<StudentCourse>,
   ) {}
+
+  async findAll(paginationDto: PaginationDto) {
+    const { limit = 3, offset = 0 } = paginationDto;
+    return this.studentCourseRepository.find({
+      take: limit,
+      skip: offset,
+    });
+  }
 
   async create(createStudentCourseDto: CreateStudentCourseDto) {
     try {
@@ -17,13 +34,63 @@ export class StudentCoursesService {
         createStudentCourseDto,
       );
       await this.studentCourseRepository.save(studentCourse);
-
       return studentCourse;
     } catch (error) {
-      console.log(error);
-      throw new InternalServerErrorException(
-        'Error al crear el registro de curso del estudiante',
-      );
+      this.handleDBException(error);
     }
+  }
+
+  async update(id: number, updateDto: Partial<CreateStudentCourseDto>) {
+    const studentCourse = await this.studentCourseRepository.findOne({
+      where: { student_courses_id: id },
+    });
+
+    if (!studentCourse) {
+      throw new NotFoundException(`Registro con id ${id} no encontrado`);
+    }
+    try {
+      this.studentCourseRepository.merge(studentCourse, updateDto);
+      await this.studentCourseRepository.save(studentCourse);
+      return {
+        message: 'Registro actualizado con éxito',
+        data: studentCourse,
+      };
+    } catch (error) {
+      this.handleDBException(error);
+    }
+  }
+
+  async remove(id: number) {
+    const exists = await this.studentCourseRepository.existsBy({
+      student_courses_id: id,
+    });
+    if (!exists) {
+      throw new NotFoundException(`Registro con id ${id} no encontrado`);
+    }
+    await this.studentCourseRepository.softDelete({ student_courses_id: id });
+    return {
+      message: `Registro con ID ${id} eliminado con éxito`,
+      deletedAt: new Date(),
+    };
+  }
+
+  async findOne(id: number) {
+    const studentCourse = await this.studentCourseRepository.findOneBy({
+      student_courses_id: id,
+    });
+    if (!studentCourse) {
+      throw new NotFoundException(`Registro con id ${id} no encontrado`);
+    }
+    return studentCourse;
+  }
+
+  private handleDBException(error: any) {
+    if (error.code === '23505') throw new BadRequestException(error.detail);
+
+    this.logger.error(error);
+
+    throw new InternalServerErrorException(
+      'Ocurrió un error inesperado. Intente más tarde',
+    );
   }
 }
