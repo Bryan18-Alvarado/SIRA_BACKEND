0f1a4cf157ccc5a867abfd3743b35b5b9c395e31
bryan_alvarado18 <117047716+Bryan18-Alvarado@users.noreply.github.com>
2025-04-29 19:46:44 -0600

    This action adds a new auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { Type } from 'class-transformer';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 
