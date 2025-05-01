f6063cd642508bb43d887bff5ded0330142dd141
bryan_alvarado18 <117047716+Bryan18-Alvarado@users.noreply.github.com>
2025-05-01 17:36:31 -0600

    feat(auth): Implementar la autenticación JWT con el registro e inicio de sesión de usuarios.
    - Se añadieron los módulos JWT y Passport para gestionar la autenticación.
    - Se crearon puntos finales de inicio de sesión y registro en AuthController.
    - Se implementó la lógica de creación e inicio de sesión de usuarios en AuthService.
    - Se añadió una estrategia JWT para validar tokens.
    - Se crearon decoradores para extraer información de los usuarios de las solicitudes.
    - Se introdujeron DTO para el registro e inicio de sesión de usuarios.
    - Se mejoró la entidad "Usuario" con hash de contraseñas y normalización de campos.
    - Se añadió la gestión de errores para las operaciones de base de datos.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,31 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateUserDto } from './dto/create-user.dto';
 
+import { CreateUserDto, LoginUserDto } from './dto';
+import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from './decorators/get-user.decorator';
+import { User } from './entities/user.entity';
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
-  create(@Body() createUserDto: CreateUserDto) {
+  createUser(@Body() createUserDto: CreateUserDto) {
     return this.authService.create(createUserDto);
+  }
+
+  @Post('login')
+  loginUser(@Body() loginUserDto: LoginUserDto) {
+    return this.authService.login(loginUserDto);
+  }
+
+  @Get('private')
+  @UseGuards(AuthGuard())
+  testingPrivateRoute(@GetUser() user: User) {
+    return {
+      ok: true,
+      message: 'hola mundo privado wasaaaaa',
+      user,
+    };
   }
 }
